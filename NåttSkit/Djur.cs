189627362e935f8c6cf89a1634e393233dc189a0
Alexander Eijvergard <alexander.eijvergard@elev.ga.ntig.se>
2024-10-01 11:18:23 +0200

    Snart klar

--- NåttSkit/Djur.cs
+++ NåttSkit/Djur.cs
@@ -1,7 +1,71 @@
-public class Djur
+using System;
+using System.Collections.Generic;
+
+public class Djuret
 {
-public string Name;
-private int Hunger;
-private int boredom;
-private bool isAlive = true;
+  private int hunger;
+  private int boredom;
+  private List<string> words = new List<string>() {"Tjena brorsan!"};
+  private bool isAlive;
+  public string name;
+
+  public Djuret()
+  {
+    isAlive = true;
+  }
+
+  public void Feed()
+  {
+    Console.WriteLine($" [{name}] Dammar och blir mätt");
+    hunger -= 2;
+    if (hunger < 0)
+    {
+      hunger = 0;
+    }
+  }
+
+  public void Hi()
+  {
+    int wordNum = Random.Shared.Next(words.Count);
+    Console.WriteLine($" [{name}] säger: {words[wordNum]}");
+    ReduceBoredom();
+  }
+
+  public void Teach(string word)
+  {
+    Console.WriteLine($" [{name}] lärde sig: {word}");
+    words.Add(word);
+    ReduceBoredom();
+  }
+
+  public void Tick()
+  {
+    hunger++;
+    boredom++;
+    if (hunger > 10 || boredom > 10)
+    {
+      isAlive = false;
+    }
+  }
+
+  public void PrintStats()
+  {
+    Console.WriteLine($"Name: {name} || Hunger: {hunger} || Boredom: {boredom} || Vocabulary: {words.Count} words");
+  }
+
+  public bool GetAlive()
+  {
+    return isAlive;
+  }
+
+  public void ReduceBoredom()
+  {
+    Console.WriteLine($" [{name}] är inte uttråkad!");
+
+    boredom -= 2;
+    if (boredom < 0)
+    {
+      boredom = 0;
+    }
+  }
 }
--- NåttSkit/Program.cs
+++ NåttSkit/Program.cs
@@ -1,1 +1,49 @@
-﻿
+﻿﻿
+Console.WriteLine("Djurslakt!");
+
+Djur myDjur = new Djur();
+
+Console.WriteLine("Välj ett namn till ditt djur!");
+myDjur.name = Console.ReadLine();
+
+Console.WriteLine($"Great! {myDjur.name} is a beautiful name!");
+Console.WriteLine("Press any key to continue");
+Console.ReadKey();
+
+while (myDjur.GetAlive() == true)
+{
+  Console.Clear();
+  myDjur.PrintStats();
+  Console.WriteLine("Now what do you want to do?");
+  Console.WriteLine($"1. Teach {myDjur.name} a new word");
+  Console.WriteLine($"2. Talk to {myDjur.name}");
+  Console.WriteLine($"3. Feed {myDjur.name}");
+  Console.WriteLine($"4. Do nothing");
+
+  string doWhat = Console.ReadLine();
+  if (doWhat == "1")
+  {
+    Console.WriteLine("What word?");
+    string word = Console.ReadLine();
+    myDjur.Teach(word);
+  }
+  if (doWhat == "2")
+  {
+    myDjur.Hi();
+  }
+  if (doWhat == "3")
+  {
+    myDjur.Feed();
+  }
+  else
+  {
+    Console.WriteLine("Doing nothing...");
+  }
+  myDjur.Tick();
+  Console.WriteLine("Press Enter to continue");
+  Console.ReadLine();
+}
+
+Console.WriteLine($"OH NO! {myDjur.name} is dead!");
+Console.WriteLine("Press ENTER to quit");
+Console.ReadLine();
